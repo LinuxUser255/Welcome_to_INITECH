7369c11d2ee790aae85f125e74a91f2a267290e3
Chris <atnl@protonmail.com>
2020-06-20 17:29:28 +0000

    Update to initech.sh
    If you've seen the movie Office Space, then this will make sense.
    This exercise is intended as a funny way to learn Bash, and identify bugs in code.

--- initech.sh
+++ initech.sh
@@ -2,10 +2,8 @@
 
 # WELCOME TO INITECH!
 # This script creates a new user on the local system.
-# You will be prompted to enter the username (login), the person name, and a password.
-# The username, password, and host for the account will be displayed.
 
-# Make sure the script is being executed with superuser privileges.
+# Make sure the script is being executed with sudo privileges.
 if [[ "${UID}" -ne 0 ]]
 then
    echo 'Please run with sudo or as root.'
@@ -16,10 +14,10 @@ fi
 read -p 'Enter the username to create: ' USER_NAME
 
 # Get the real name (contents for the description field).
-read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
+read -p 'Enter the name of the person that will be using this account: ' COMMENT
 
 # Get the password.
-read -p 'Enter the password to use for the account: ' PASSWORD
+read -p 'Enter the user's password: ' PASSWORD
 
 # Create the account.
 useradd -c "${COMMENT}" -m ${USER_NAME}
@@ -27,7 +25,6 @@ useradd -c "${COMMENT}" -m ${USER_NAME}
 echo 'Welcome to INITECH!'
 
 # Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
 if [[ "${?}" -ne 0 ]];
 then
   echo 'The account could not be created.'
@@ -40,5 +37,10 @@ fi
 
 # Set the password.
 echo "${USER_NAME}" 
+
+#Now the challenge.
+#Put a bug in Lumbergh's account ;)
+#Fork it, edit, pull it.
+
 
 
